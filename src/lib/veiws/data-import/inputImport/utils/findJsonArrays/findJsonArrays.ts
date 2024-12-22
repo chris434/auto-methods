1f30aace192c3befd97da6564babfb0e56e80254
chris pearce <chrispearce@chriss-Laptop.local>
2024-12-22 13:33:13 +0000

    refact findJsonArray use forEachObject

--- src/lib/veiws/data-import/inputImport/utils/findJsonArrays/findJsonArrays.ts
+++ src/lib/veiws/data-import/inputImport/utils/findJsonArrays/findJsonArrays.ts
@@ -1,19 +1,17 @@
+import { forEachObject } from "$lib/utitls/object/forEachObject"
 import { typeofValue } from "$lib/utitls/typeof/typeof"
 
 export function findJsonArrays(data:object ){
 const arrays:[string,unknown[]][]=[]
 function find(data:object,path:string){
-    Object.entries(data).forEach((entry) =>{
-        const [key,value]=entry
+    forEachObject(data,(key,value)=>{
         const currentPath=`${path}/${key}`
-       
         if(Array.isArray(value)) arrays.push([currentPath,value])
          if(typeofValue(value)==='object')return find(value as object,currentPath)
 
-        
-     })
+    })
 }
 find(data,'')
-console.log(arrays)
+
 return arrays
 } 
