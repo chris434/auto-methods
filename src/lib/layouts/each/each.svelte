326ad4bc27e9429a6be7f110a6bc03bb4069442e
chris pearce <chrispearce@chriss-Laptop.local>
2025-01-26 14:00:21 +0000

    refact get moved to separate file

--- src/lib/layouts/each/each.svelte
+++ src/lib/layouts/each/each.svelte
@@ -2,9 +2,10 @@
 
 <script lang="ts" generics="T extends ActionKeys">
     import type {ActionKeys,SelectedProp, GetReturnType} from '$lib/state/arrayData/types'
-	import { arrayData,get } from '$lib/state/arrayData/arrayData.svelte';
+	import { arrayData } from '$lib/state/arrayData/arrayData.svelte';
 	import type { Snippet } from 'svelte';
 	import { getProperty } from './helpers/getProperty';
+	import { get } from './helpers/get';
 let {key,props,children}:{key:T,props:SelectedProp<T>,children:Snippet<[child:GetReturnType<T>,sindex:number]>}=$props()
 let data= $derived(key=="methods"&&{methods:arrayData.data.methods}||props&& get<T>(props))
 console.log(props&&get<T>(props))
